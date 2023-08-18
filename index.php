c11588e8325e3f1701a86ddc43d1ec794281bcef
Anthony <luis.anthony.laml@gmail.com>
2023-08-18 16:40:45 -0500

    Agregado de navegación e inicio, se incluye el css

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMC | Online Store</title>
+    
+    <!-- FAvicon -->
+    <link rel="shortcut icon" href="img/ico.svg" type="image/x-icon">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Nav -->
+        <div class="nav container">
+            <!-- Logo -->
+            <a href="index.php" class="logo">
+                <img src="img/ico.svg" alt="logo cmc"><span>CMC</span>
+            </a>
+            <!-- Navbar -->
+            <ul class="navbar">
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#nuevo">Nuevo</a></li>
+                <li><a href="#productos">Productos</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- Sección de inicio -->
+    <section class="inicio" id="inicio">
+        <div class="inicio-texto">
+            <h1>CMC</h1>
+            <h2>Estilo que deja huellas</h2>
+            <a href="#productos" class="btn">Ver productos</a>
+        </div>
+        <div class="inicio-imagen">
+            <img src="img/ico.svg" alt="logo cmc">
+        </div>
+    </section>
+</body>
+</html>
